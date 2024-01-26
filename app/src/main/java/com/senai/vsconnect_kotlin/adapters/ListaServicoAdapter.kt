abaa5219e60becce9b976ca93ef3891360ee506f
Denis luan Sales de Santana <394.299.878-52@info.local>
2024-01-25 21:53:14 -0300

    okk

--- app/src/main/java/com/senai/vsconnect_kotlin/adapters/ListaServicoAdapter.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/adapters/ListaServicoAdapter.kt
@@ -1,4 +1,42 @@
 package com.senai.vsconnect_kotlin.adapters
 
-class ListaServicoAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.senai.vsconnect_kotlin.R
+import com.senai.vsconnect_kotlin.models.Servico
+
+class ListaServicoAdapter(
+    private val context: Context,
+    private val listaServicos: List<Servico>
+) : RecyclerView.Adapter<ListaServicoAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        //Essa funçao e responsavel por chamar e atribuir valores para views do item RecylceView
+        fun vincularDadosNoItem(servico: Servico) {
+            val tituloServico = itemView.findViewById<TextView>(R.id.nomeServico)
+            tituloServico.text = servico.titulo
+
+            val propostaServico = itemView.findViewById<TextView>(R.id.valorServico)
+            propostaServico.text = servico.proposta
+
+            val descricaoServico = itemView.findViewById<TextView>(R.id.descricaoServico)
+            descricaoServico.text = servico.descricao
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaServicoAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListaServicoAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
